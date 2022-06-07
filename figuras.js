4672518386420f2ea5c57c9058debbc3e7cd3f79
jhangmez <jhangomez25@gmail.com>
2022-06-06 19:43:16 -0500

    Termine de agregar las funciones, js e interacciones entre los 2

--- figuras.js
+++ figuras.js
@@ -53,12 +53,13 @@ console.groupEnd();
 
 //Aqui comenzamos la interaccion con el html
 
+//Cuadrado
 function calcularperimetroCuadrado(){
     const input = document.getElementById("inputCuadrado");
     const value = input.value;
 
     const perimetro = perimetroCuadrado(value);
-    alert(perimetro);
+    alert("El perimetro del cuadrado es " +perimetro);
 }
 
 function calcularareaCuadrado(){
@@ -66,5 +67,54 @@ function calcularareaCuadrado(){
     const value = input.value;
 
     const area = areaCuadrado(value);
-    alert(area);
+    alert("El area del cuadrado es "+area);
+}
+
+//Triangulo
+
+function calcularperimetroTriangulo(){
+    const input1 = document.getElementById("inputTriangulo1");
+    const input2 = document.getElementById("inputTriangulo2");
+    const base = document.getElementById("inputTriangulo3");
+    
+    const value1 = Number(input1.value);
+    const value2 = Number(input2.value);
+    const value3 = Number(base.value);
+    
+    const perimetro = perimetroTriangulo(value1,value2, value3);
+
+    alert("El perimetro del triangulo es " +perimetro);
+}
+
+function calcularareaTriangulo(){
+    
+    const base = document.getElementById("inputTriangulo3");
+    const altura = document.getElementById("inputTriangulo4");
+    
+    const value3 = Number(base.value);
+    const value4 = Number(altura.value);
+
+    const area = areaTriangulo(value3,value4);
+
+    alert("El area del triangulo es "+area);
+}
+
+//Circunferencia
+
+function calcularperimetroCircunferencia(){
+    const input1 = document.getElementById("inputCircunferencia");
+    const value = input1.value;
+    
+    const perimetro = perimetroCircunferencia(value);
+
+    alert("El perimetro de la circunferencia es " +perimetro);
+}
+
+function calcularareaCircunferencia(){
+    const input1 = document.getElementById("inputCircunferencia");
+    const value = input1.value;
+    
+    const area = areaCircunferencia(value);
+
+    alert("El area de la circunferencia es "+area);
 }
